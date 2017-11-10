68d17f4b09360937335f16085b2fe841e20561dc
peng <liupeng3425@qq.com>
2017-11-10 11:09:14 +0800

    minst_fully_connected_feed

---
+++ vector/numpy_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+import scipy
+import time
+
+a = np.array([[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]])
+b = np.array([[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]])
+
+tf.nn.conv2d(padding='SA')
+
+print(b.shape)
+
+
