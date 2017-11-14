f9d22497dc3d798aa7c1c5589e8ae6bae43576cd
peng <liupeng3425@qq.com>
2017-11-14 21:43:11 +0800

    print test accuracy

--- mnist/minst_cnn.py
+++ mnist/minst_cnn.py
@@ -65,11 +65,9 @@ sess.run(tf.global_variables_initializer())
 for i in range(10000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
+        train_accuracy, loss = sess.run([accuracy, cross_entropy], feed_dict={
             x: batch[0], y_label: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-        print("test accuracy %g" % accuracy.eval(feed_dict={
-            x: mnist.test.images, y_label: mnist.test.labels, keep_prob: 1.0}))
+        print("step %d, training accuracy %g, loss %g" % (i, train_accuracy, loss))
     train_step.run(feed_dict={x: batch[0], y_label: batch[1], keep_prob: 0.5})
 
 print("test accuracy %g" % accuracy.eval(feed_dict={
